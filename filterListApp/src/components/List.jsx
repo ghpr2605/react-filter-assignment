76cd4605c9b39d5a43923afeac1def1c9854fbda
Pranay Ghiya <50547453+ghpr2605@users.noreply.github.com>
2022-07-25 23:11:04 +0530

    added index key for iterating

--- filterListApp/src/components/List.jsx
+++ filterListApp/src/components/List.jsx
@@ -3,8 +3,8 @@ const List = ({ data }) => {
     <>
       <h2>List is shown as below</h2>
       <ul>
-        {data.map((item) => {
-          return <li>{item}</li>;
+        {data.map((item, index) => {
+          return <li key={`${index}_${item}`}>{item}</li>;
         })}
       </ul>
     </>
