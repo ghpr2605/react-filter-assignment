4fe2d5ce519bbbd78ffedfa1b3b5996522ef81dc
Pranay Ghiya <50547453+ghpr2605@users.noreply.github.com>
2022-07-26 00:08:44 +0530

    Update List.jsx

--- filterListApp/src/components/List.jsx
+++ filterListApp/src/components/List.jsx
@@ -1,9 +1,29 @@
+import { useState } from "react";
+
 const List = ({ data }) => {
+  const [list, updateList] = useState(data);
+  const filterList = (event) => {
+    const keyWord = event.target.value;
+    const filteredList = keyWord
+      ? list.filter((item) => {
+          return item.includes(keyWord);
+        })
+      : data;
+
+    updateList(filteredList);
+  };
+
   return (
     <>
+      <h2>Text box for filter</h2>
+      <input
+        type="text"
+        placeholder="Enter key word for filter"
+        onChange={filterList}
+      />
       <h2>List is shown as below</h2>
       <ul>
-        {data.map((item, index) => {
+        {list.map((item, index) => {
           return <li key={`${index}_${item}`}>{item}</li>;
         })}
       </ul>
